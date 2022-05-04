39b09f06133b7e76fcfb578561b2cd2ec578f0c7
Josue JUMPSTONIK Lopez <Josue.Lopez@telus.com>
2022-05-04 00:44:22 -0600

    fix & feat: fixing names of labels of update users and adding the password change screen

---
+++ src/containers/PasswordChange/index.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import { Box, Container, TextField, Avatar, Input, Button, Stack} from '@mui/material'
+import { styles } from '../../utils/styles';
+import { NavigateButton } from '../../components/NavigateButton';
+
+export const PasswordChange = () => {
+    return (
+        <Container>
+            <Box sx={styles.EditUserInfo}>
+            <Box sx={styles.Form}>
+                <Box sx={styles.btnReturnEditUser}>
+                    <NavigateButton btnColor1='#7D506B' btnColor2='#906D89' content='return' path='/settings'/>
+                </Box>
+                
+                <TextField
+                required
+                id="filled-required"
+                label="Old password"
+                variant="filled"
+                type='password'
+                sx={{width: '100%'}}
+                />
+                <TextField
+                required
+                id="filled-required"
+                label="New password"
+                variant="filled"
+                type='password'
+                sx={{width: '100%'}}
+                />
+                <Button variant='contained'>Change password</Button>
+            </Box>
+            </Box>
+        </Container>
+    )
+}
